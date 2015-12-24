91f7c41d0a3f3275676fb31ea6a886fec7a3b336
AlissaSquared <vandor2012@gmail.com>
2015-12-24 00:24:53 -0600

    Hello World

---
+++ compile.sh
@@ -0,0 +1,6 @@
+for file in $(find . -type f -name "*.moon"); do
+	luafile="${file%.*}.lua"
+	if [ ! -f "$luafile" ] || [ $(stat -c "%Y" "${file}" ) -gt $(stat -c "%Y" "$luafile") ]; then
+		moonc $file
+	fi
+done
