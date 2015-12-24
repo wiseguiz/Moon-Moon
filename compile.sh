a74d10f220cd8358279fc65b6b4121a7ceff78aa
AlissaSquared <vandor2012@gmail.com>
2015-12-24 01:20:29 -0600

    Improvements to compiler and irc library

--- compile.sh
+++ compile.sh
@@ -2,5 +2,6 @@ for file in $(find . -type f -name "*.moon"); do
 	luafile="${file%.*}.lua"
 	if [ ! -f "$luafile" ] || [ $(stat -c "%Y" "${file}" ) -gt $(stat -c "%Y" "$luafile") ]; then
 		moonc $file
+		luac -o $luafile $luafile
 	fi
 done
--- main.moon
+++ main.moon
@@ -1,1 +1,1 @@
-print 'Hello World'
+require 'irc'
