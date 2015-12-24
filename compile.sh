31546be9dcfffc7e29e58ada547e91066c4a8d66
AlissaSquared <vandor2012@gmail.com>
2015-12-24 01:36:12 -0600

    more startup stuff, entrypoint is now start.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,15 @@
+has_compiled=false
 for file in $(find . -type f -name "*.moon"); do
 	luafile="${file%.*}.lua"
 	if [ ! -f "$luafile" ] || [ $(stat -c "%Y" "${file}" ) -gt $(stat -c "%Y" "$luafile") ]; then
+		if ! $has_compiled; then
+			printf "\n"
+		fi
 		moonc $file
 		luac -o $luafile $luafile
+		has_compiled=true
 	fi
 done
+if $has_compiled; then
+	printf "\n"
+fi
