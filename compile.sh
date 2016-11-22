9cb657753b813b9b0812d2ff611dc34e1445a3f5
RyanSquared <vandor2012@gmail.com>
2016-11-21 22:50:22 -0600

    compile.sh: refactor file preprocessor and add help flag

--- compile.sh
+++ compile.sh
@@ -1,35 +1,37 @@
-if which filepp; then
-	CPP="filepp"
-elif which cpp; then
-	CPP="cpp -P"
-else
-	echo "No C preprocessor found"
-	exit 1
-fi
-testfunc(){
-	if ! which $1; then
-		echo "$1 not found"
-		exit 1
-	fi
-}
-
-for func in moonc luac; do
-	testfunc $func
-done
-
 has_compiled=false
 is_binary=false
 is_forcing=false
 for arg in $*; do
 	case $arg in
-		-binary)
+		--bytecode|-b)
 			is_binary=true
 			;;
-		-force-compile)
+		--force-compile|-f)
 			is_forcing=true
 			;;
+    --help|-h)
+      echo "Usage: ./compile.sh [OPTIONS]"
+      echo "Compile MoonScript files into Lua files - binary or source"
+      echo ""
+      echo "  -f, --force-compile   Force recompilation of all files"
+      echo "  -b, --bytecode        Files are compiled to Lua bytecode"
+      echo "  -h, --help            Display this message"
+      exit 0
+      ;;
 	esac
 done
+
+find_command() {
+  while (( "$#" )); do
+    echo "Looking for... $1"
+    which $1 >/dev/null 2>&1 || exit $?
+    echo "Using $(which $1)"
+    shift
+  done
+}
+
+find_command moonc luac
+
 for file in $(find . -type f -name "*.moon"); do
 	luafile="${file%.*}.lua"
 	if [ ! -f "$luafile" ] || [ $(stat -c "%Y" "${file}" ) -gt $(stat -c "%Y" "$luafile") ] || $is_forcing; then
